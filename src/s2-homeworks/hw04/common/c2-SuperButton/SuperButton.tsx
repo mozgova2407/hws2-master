34578a53282a62a1322a7cedea2e753360fe12ef
mozgova2407 <mozgova2407@mail.ru>
2026-03-27 19:25:35 +0300

    изменения в супербатон

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,13 +19,12 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName =
         s.button +
-        (disabled
-            ? ' ' + s.disabled
-            : xType === 'red'
-                ? ' ' + s.red
-                : xType === 'secondary'
-                    ? ' ' + s.secondary
-                    : ' ' + s.default) +
+        (xType === 'red'
+            ? ' ' + s.red
+            : xType === 'secondary'
+                ? ' ' + s.secondary
+                : ' ' + s.default) +
+        (disabled ? ' ' + s.disabled : '') +
         (className ? ' ' + className : '')
 
     return (
